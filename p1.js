25314b98a18c16d4d112dfb1a12b66bb6d593844
Iván <pillado.rodriguez.ivan@gmail.com>
2024-10-13 13:48:14 +0200

    añadida funcion enviarFormulario y sus respectivos cambios en html

--- p1.js
+++ p1.js
@@ -1,6 +1,8 @@
-const botonSubmit = document.getElementById("b_enviar");
+//const botonSubmit = document.getElementById("b_enviar");
 
-function enviar(){
+function enviarFormulario(){
+    const infoNavegador = obtenerInfoNavegador();
+    document.getElementById("cinfo").value = infoNavegador;
     comprobar();
 
 }
@@ -33,5 +35,23 @@ function check_dni(dni){
 }
 
 function check_password(password){
-    const pattern = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,12}$/;
+    const pattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\+\-\*])[A-Za-z\d\+\-\*]{6,12}$/;
+    if(pattern.test(password) == false){
+        alert("La contraseña no es válida\n\nDebe contener al menos:\n- 1 número\n- Una mayúscula y minúscula\n- Un símbolo (+,-,/,*)\n- Debe tener entre 6 y 12 caracteres");
+        return false;
+    }
+    return true;
+}
+
+function check_login(login){
+    let length = login.length;
+    if(length < 4 || length > 8){
+        alert("El login debe tener entre 4 y 8 caracteres");
+        return false;
+    }
+    return true;
+}
+
+function obtenerInfoNavegador(){
+    return navigator.userAgent;
 }
